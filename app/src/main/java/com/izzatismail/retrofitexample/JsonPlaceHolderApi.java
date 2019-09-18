daa4ac3f6ad7ba503fb305e737585c4ac0053418
izzatismail <mizzat1996@gmail.com>
2019-09-18 16:31:40 +0800

    Added GET & QUERY notes

--- app/src/main/java/com/izzatismail/retrofitexample/JsonPlaceHolderApi.java
+++ app/src/main/java/com/izzatismail/retrofitexample/JsonPlaceHolderApi.java
@@ -1,13 +1,44 @@
 package com.izzatismail.retrofitexample;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
 
 //The name "JsonPlaceHolder" will change according to where you get JSON file from.
 public interface JsonPlaceHolderApi {
 
+    /*
     @GET("posts") //Get the relative URL, the one after the base URL (/)
-    Call<List<Post>> getPosts();
+    Call<List<Post>> getPosts(); */
+
+    /* Without Sorting
+    // for /posts?userId=1
+    @GET("posts") //Get the relative URL, the one after the base URL (/)
+    Call<List<Post>> getPosts(@Query("userId") int userId); //the annotation name "userId" must be same with the one JSON */
+
+    // With Sorting
+    @GET("posts") //Get the relative URL, the one after the base URL (/)
+    Call<List<Post>> getPosts(
+            @Query("userId") Integer[] userId, //If you want to query more than 1 userId
+            @Query("_sort") String sort,
+            @Query("_order") String order
+    ); //the annotation name "userId" must be same with the one JSON
+
+    //For any combination of query
+    @GET("posts")
+    Call<List<Post>> getPosts(
+            @QueryMap Map<String, String> parameters
+            );
+
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getComments(@Path("id") int postId); //the annotation name "id" must be same with the one in the @GET {}
+
+    @GET
+    Call<List<Comment>> getComments(@Url String url); //If you want to GET by url
 }
--- app/src/main/java/com/izzatismail/retrofitexample/MainActivity.java
+++ app/src/main/java/com/izzatismail/retrofitexample/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -16,6 +18,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     private TextView textResult;
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +32,54 @@ public class MainActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class); //Retrofit will take care of the implementation
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class); //Retrofit will take care of the implementation
 
-        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+        //getPosts();
+        getComments();
+    }
+
+    private void getComments() {
+        Call<List<Comment>> call = jsonPlaceHolderApi.getComments("posts/1/comments"); //Can use URL to get the JSON
+
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+                if(!response.isSuccessful()){
+                    //If not successful response (ERROR 404 for example)
+                    textResult.setText("Code : " + response.code());
+                    return;
+                }
+
+                List<Comment> comments = response.body();
+
+                for(Comment comment : comments){
+                    String content = "";
+                    content += "ID : " + comment.getId() + "\n";
+                    content += "Post ID : " + comment.getPostId() + "\n";
+                    content += "Name : " + comment.getName() + "\n";
+                    content += "Email : " + comment.getEmail() + "\n";
+                    content += "Body : " + comment.getText() + "\n\n";
+
+                    textResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
+                textResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void getPosts(){
+        //For any combination
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("userId", "1");
+        parameters.put("_sort", "id");
+        parameters.put("_order", "desc");
+
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(parameters);
+        //Call<List<Post>> call = jsonPlaceHolderApi.getPosts(new Integer[]{2,3,6}, "id", "desc"); //use null if you don't want to be specific
 
         call.enqueue(new Callback<List<Post>>() {
             @Override
